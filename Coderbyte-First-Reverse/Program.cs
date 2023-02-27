bfccc0801e0d17375075d176413f7b41cf0a25ee
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-02-27 23:58:10 +0300

    Coderbyte -First Reverse Challenge çözüldü.

---
+++ Coderbyte-First-Reverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Coderbyte_First_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // keep this function call here
+            Console.WriteLine(FirstReverse(Console.ReadLine()));
+        }
+
+        public static string FirstReverse(string str)
+        {
+           // code goes here
+
+           string reverseText = "";
+           for (int i = str.Length - 1; i>=0 ; i--)
+           {
+                reverseText += str[i];
+           }
+           return reverseText;
+        }
+
+        // sayitsamikoca - Happy coding :)
+    }
+}
