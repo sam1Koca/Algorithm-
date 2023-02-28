dedba17ad6bcbc0c744929f1c01cfacfeb853e45
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-03-01 00:39:27 +0300

    Coderbyte First Factorial Challenge çözüldü

---
+++ Coderbyte-First-Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Coderbyte_First_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // keep this function call here
+            Console.WriteLine(FirstFactorial(Console.ReadLine()));
+        }
+
+        static int FirstFactorial(int num)
+        {
+            // code goes here
+
+            int result = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
