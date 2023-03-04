91e1e6a5038390f7c005d448fa152b9dc1471c56
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-03-04 19:37:49 +0300

    Coderbyte FindIntersection challenge solved

--- Coderbyte-Find-Intersection/Program.cs
+++ Coderbyte-Find-Intersection/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Coderbyte_Find_Intersection
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // keep this function call here
+            Console.WriteLine(FindIntersection(Console.ReadLine()));
+        }
+
+        public static string FindIntersection(string[] strArr)
+        {
+            // code goes here
+            List<int> results = new List<int>();
+
+            int[] arrayOne = strArr[0].Split(",").Select(x => int.Parse(x)).ToArray();
+            int[] arrayTwo = strArr[1].Split(",").Select(x => int.Parse(x)).ToArray();
+
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                if (arrayTwo.Contains(arrayOne[i]))
+                {
+                    results.Add(arrayOne[i]);
+                }
+            }
+            return string.Join("," , results.ToArray());
         }
     }
 }
